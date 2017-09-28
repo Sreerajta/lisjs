b7b7c9bd965b99aeb3248dc0cef14b5d274a080a
Sreeraj <Sreeraj@disroot.org>
2017-09-28 16:09:33 +0530

    m4

--- lisjs.js
+++ lisjs.js
@@ -1,13 +1,10 @@
     var http = require('http');
     http.createServer(function (req, res) {
       res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('Hello World\n');
+      res.end('Lis JS\n');
     }).listen(1337, "127.0.0.1");
 	
 //tokenizing and parsing:
-	
-	var cst="(begin(define r 10)(* pi(* r r)))";
-	
 	function tokenize(inpstring){
 	inpstring=inpstring.replace(/\(/g, ' ( ').replace(/\)/g, ' ) ').replace(/\s+/g, ' ').trim().split(' ');
 	return inpstring;
@@ -43,38 +40,29 @@
 };
 
  function parse (program) {
-    return read_from(tokenize(program));
+    return read_from_tokens(tokenize(program));
 };
 
 
 //Environment :
 function create_env(props_obj){
 	var env={};
-	if (props_obj.par_scope=='undefined'){
-		var par_scope={};
-	}
-	else{
-		var par_scope=props_obj.par_scope;
-	}
+	env.par_scope = props_obj.par_scope || {};
 	
-    
 	function ret_pscope(){
 	  return par_scope;
-	
 	};
-	 env.ret_pscope=ret_pscope;
-	
+	 
 	function search_var(variable){
-	if(env.hasOwnProperty(variable)){
+	 if(env.hasOwnProperty(variable)){
 		return env;
-	} 
+	}
 	else{
-	return par_scope.search_var(variable);
+	return env.par_scope.search_var(variable);
 	}
 	};	
 
-   
-	env.search_var=search_var;	
+  
 	
     if(props_obj.variables.length!==0){
 		for(var i=0;i<props_obj.variables.length;i+=1){
@@ -82,14 +70,27 @@ function create_env(props_obj){
 		}
 	}
 	
-	
+	env.ret_pscope=ret_pscope;
+	env.search_var=search_var;	
 	
 	return env;
 	
 };
 
 function init_std_env(env){
-    env['+'] = function(x,y){return x+y;};
+    
+	
+	env['sin']=Math['sin'];
+	env['cos']=Math['cos'];
+	env['tan']=Math['tan'];
+	env['max']=Math['max'];
+	env['min']=Math['min'];
+	env['pow']=Math['pow'];
+	env['sqrt']=Math['sqrt'];
+	env['log']=Math['log'];
+	env['exp']=Math['exp'];
+	
+	env['+'] = function(x,y){return x+y;};
     env['-'] = function(x,y){return x-y;};
     env['*'] = function(x,y){return x*y;};
     env['/'] = function(x,y){return x/y;};
@@ -105,14 +106,24 @@ function init_std_env(env){
     env['car'] = function (x) { return (x.length !== 0) ? x[0] : null; };
     env['cdr'] = function (x) { return (x.length > 1) ? x.slice(1) : null; }; 
 	env['append'] = function (x, y) { return x.concat(y); };
-    env['list'] = ?
-	env['list?'] = ?
-	env['null?'] = ?
+    env['list'] = function () { return Array.prototype.slice.call(arguments); };
+	env['list?'] =  function (x) { return x && typeof x === 'object' && x.constructor === Array ; };
+	env['null?'] = function (x) { return (!x || x.length === 0); };
 	env['symbol?'] = function (x) { return typeof x === 'string'; };
+	
     return env;
 };
 
-var global_env=init_std_env(create_env ({variables: [], vals: [], par_scope: undefined}));
+var global_env=init_std_env(create_env ({variables: ['b'], vals: [3], par_scope: undefined}));
+//var ev2=init_std_env(create_env ({variables: ['a'], vals: [1], par_scope:global_env}));
+//var evf=init_std_env(create_env ({variables: ['x'], vals: [1], par_scope:ev2}));
+//ev3=evf.search_var('x');
+//console.log(ev3);
+
+
+
+
+
 
 
 //eval:
@@ -121,7 +132,7 @@ function eval (x, env) {
 	env = env || global_env;
 
     if (typeof x === 'string') {	
-        return env.find(x.valueOf())[x.valueOf()];
+        return env.search_var(x.valueOf())[x.valueOf()];
     } else if (typeof x === 'number') {	
         return x;
     } else if (x[0] === 'quote') {	
@@ -143,7 +154,7 @@ function eval (x, env) {
         var vars = x[1];
         var exp = x[2];
         return function () {
-	        return eval(exp, environment({variables: vars, vals: arguments, par_scope: env }));
+	        return eval(exp, create_env({variables: vars, vals: arguments, par_scope: env }));
         };
     } else if (x[0] === 'begin') {	
         var val;
@@ -161,3 +172,8 @@ function eval (x, env) {
     }
 };
 
+eval(parse("(define r 10)"));
+eval(parse("(define pi 3.14)"));
+var xx=eval(parse("(* pi (* r r))"));
+console.log(xx);
+process.exit()
